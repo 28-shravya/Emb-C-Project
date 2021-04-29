dd09b2850cf0270b586802b63275fd5eeb941bb4
28-shravya <66370715+28-shravya@users.noreply.github.com>
2021-04-29 11:14:08 +0530

    modified

--- inc/Activity4.h
+++ inc/Activity4.h
@@ -12,6 +12,8 @@
 #ifndef ACTIVITY4_H_INCLUDED
 #define ACTIVITY4_H_INCLUDED
 
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+
 #include<avr/io.h>
 #include <util/delay.h>
 #include <stdint.h>
